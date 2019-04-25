b7b1960bd3c50a40e68b10c3fef6293f467ecb10
khanhtungmtp <umlatao97@gmail.com>
2019-04-25 16:58:14 +0700

    chuyển đổi giữa các components

--- section 09/learnwords/src/App.vue
+++ section 09/learnwords/src/App.vue
@@ -3,12 +3,21 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-12">
-<!--    card-words là props bên card.vue      -->
+         <button class="btn btn-outline-danger" @click="selectedComponents = 'appCard'">Card</button>
+         <button class="btn btn-outline-danger" @click="selectedComponents = 'appAbout'">About</button>
+         <button class="btn btn-outline-danger" @click="selectedComponents = 'appContact'">Contact</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+          <!--    card-words là props bên card.vue      -->
           <app-card card-words="Hello props">
             <h1 slot="title">{{ whiteCard }}</h1>
             <p slot="content">Giữ chỗ bên trong app-card bằng thẻ slot bên Card.Vue</p>
 
           </app-card>
+<component :is="selectedComponents"></component>
+
         </div>
       </div>
     </div>
@@ -17,15 +26,20 @@
 
 <script>
   import Card from './components/Card.vue';
+  import About from './components/About.vue'
+  import Contact from './components/Contact.vue'
 
   export default {
-    data:function () {
+    data: function () {
       return {
-        whiteCard:'Thử thêm mới data trong slot có hoạt động như bình thường'
+        whiteCard: 'Thử thêm mới data trong slot có hoạt động như bình thường',
+        selectedComponents:'appCard'
       }
     },
     components: {
-      appCard: Card
+      appCard: Card,
+      appAbout:About,
+      appContact:Contact
     }
   }
 </script>
---
+++ section 09/learnwords/src/components/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <p>this is contact page</p>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
