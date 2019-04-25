1f6a5839815eab1340d13fa2ee50663ce798aa03
khanhtungmtp <umlatao97@gmail.com>
2019-04-25 12:52:06 +0700

    Thiết lập một module đơn giản

---
+++ section 09/learnwords/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <app-card>
+
+          </app-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Card from './components/Card.vue';
+
+  export default {
+    components: {
+      appCard: Card
+    }
+  }
+</script>
+
+<style>
+
+</style>
