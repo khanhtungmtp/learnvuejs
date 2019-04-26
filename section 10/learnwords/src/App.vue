f72c9d44eaacf83d7dfcaad90124a8c7b04e857d
khanhtungmtp <umlatao97@gmail.com>
2019-04-26 12:05:13 +0700

    init section 10

---
+++ section 10/learnwords/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+  import Card from './components/Card.vue';
+  import About from './components/About.vue'
+  import Contact from './components/Contact.vue'
+
+  export default {
+    data: function () {
+      return {
+        items: 'data item section 10',
+        maxItem: 10
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
