8c8d3aeb02a838dbe871884a7d59bf4c13d95fd8
khanhtungmtp <umlatao97@gmail.com>
2019-04-26 17:56:05 +0700

    finish procesing bar

--- section 10/learnwords/src/App.vue
+++ section 10/learnwords/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="container">
+    <app-header :itemCount="items.length" :maxItem="maxItemData"></app-header>
     <app-item-layout :itemsProps="items" @xoaItem="removeItem"></app-item-layout>
     <!-- @itemAdded là emit bên newItem.vue submit form gửi qua, addItem là method   -->
-    <app-new-item @itemAdded="addItem"></app-new-item>
+    <app-new-item @itemAdded="addItem" ></app-new-item>
     <div class="row">
       <div class="col-sm-12 text-center">
         <div class="alert alert-danger">
@@ -16,25 +17,33 @@
 <script>
   import ItemLayout from './components/ItemLayout.vue';
   import NewItem from './components/NewItem.vue';
+  import Header from './components/Header.vue';
 
   export default {
     data: function () {
       return {
         items: []
         ,
-        maxItem: 10
+        maxItemData: 10
       }
     },
     components: {
       // (giống đăng ký thẻ html)tạo components rồi gán vào để sử dụng được thẻ app-item-layout
       appItemLayout: ItemLayout,
-      appNewItem: NewItem
+      appNewItem: NewItem,
+      appHeader:Header
     },
     methods: {
       // parameter item lưu lại giá trị mà form đã nhập bên form newItem.vue
       addItem(item) {
-        // thêm vào mảng items data bên trên
-        this.items.push(item);
+        if (this.items.length >= this.maxItemData){
+          return alert('maximum item rồi');
+        }else if(this.items.includes(item)){
+          return alert('existed item');
+        } else{
+          // thêm vào mảng items data bên trên
+          this.items.push(item);
+        }
       },
       removeItem(index){
         // truy cập vào mảng
---
+++ section 10/learnwords/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12">
+      <span>Item added</span>
+      <div class="progress">
+        <div class="progress-bar bg-success" :style="{width: (itemCount/ maxItem)*100 +'%'}">
+         {{ itemCount}} / {{ maxItem }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    //  hứng giá trị cha
+    props: ['itemCount', 'maxItem']
+  }
+</script>
+
+<style scoped>
+
+</style>
