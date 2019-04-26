bd36f143950e2f09762ea2538e4bf194fd139360
khanhtungmtp <umlatao97@gmail.com>
2019-04-26 13:21:16 +0700

    thêm mới dữ liệu vào mảng items

--- section 10/learnwords/src/App.vue
+++ section 10/learnwords/src/App.vue
@@ -1,26 +1,36 @@
 <template>
   <div class="container">
     <app-item-layout :itemsProps="items"></app-item-layout>
+<!-- @itemAdded là emit bên newItem.vue submit form gửi qua, addItem là method   -->
+    <app-new-item @itemAdded="addItem"></app-new-item>
   </div>
 </template>
 
 <script>
   import ItemLayout from './components/ItemLayout.vue';
+  import NewItem from './components/NewItem.vue';
 
   export default {
     data: function () {
       return {
         items: [
-          'data item section 10',
-          'hello world'
+
         ]
         ,
         maxItem: 10
       }
     },
     components:{
-      // tạo components rồi gán vào để sử dụng được thẻ app-item-layout
-      appItemLayout:ItemLayout
+      // (giống đăng ký thẻ html)tạo components rồi gán vào để sử dụng được thẻ app-item-layout
+      appItemLayout:ItemLayout,
+      appNewItem:NewItem
+    },
+    methods:{
+      // parameter item lưu lại giá trị mà form đã nhập bên form newItem.vue
+      addItem(item){
+        // thêm vào mảng items data bên trên
+        this.items.push(item);
+      }
     }
   }
 </script>
--- section 10/learnwords/src/components/NewItem.vue
+++ section 10/learnwords/src/components/NewItem.vue
@@ -1,26 +1,33 @@
 <template>
-    <div class="row">
-      <form>
-        <div class="col-sm-8 sm-offset-2 col-md-6 form-group">
-          <label>Title item</label>
-          <textarea v-model="item" name="" id="" cols="30" rows="10"></textarea>
-        </div>
-        <div class="col-sm-8 sm-offset-2 col-md-6 form-group">
+  <div class="row">
+    <form>
+      <div class="col-sm-8 sm-offset-2 col-md-6 form-group">
+        <label>Title item</label>
+        <textarea v-model="item" name="" id="" cols="30" rows="10"></textarea>
+      </div>
+      <div class="col-sm-8 sm-offset-2 col-md-6 form-group">
         <!--   @click.prevent ngăn ko cho load lại trang       -->
-          <button class="btn btn-success" @click.prevent="createNewItem">Create new item</button>
-        </div>
-      </form>
-    </div>
+        <button class="btn btn-success" @click.prevent="createNewItem">Create new item</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {
-        data:function () {
-          return {
-            item:''
-          }
-        }
+  export default {
+    data: function () {
+      return {
+        item: ''
+      }
+    },
+    methods: {
+      createNewItem() {
+        this.$emit('itemAdded', this.item);
+        //   cho rỗng khi đã add xong
+        this.item = '';
+      }
     }
+  }
 </script>
 
 <style scoped>
