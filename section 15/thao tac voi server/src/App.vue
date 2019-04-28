ea3c8c0d7c7a8a623b5721b4427fe1570c0aa91c
khanhtungmtp <umlatao97@gmail.com>
2019-04-28 21:22:10 +0700

    post với vue resource và root đường dẫn database

---
+++ section 15/thao tac voi server/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="form-group">
+          <h1>làm việc với database firebase</h1>
+          <input type="text" class="form-control" v-model="user.username">
+          <input type="text" class="form-control" v-model="user.email">
+          <button @click="submit" class="btn btn-success">Tạo mới</button>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data(){
+      return {
+        user:{
+          username :'',
+          email:''
+        }
+      }
+    },
+    methods:{
+      submit(){
+        // console.log(this.user)
+        this.$http.post("",this.user)
+          .then(response =>{
+            console.log(response)
+          },error =>{
+            console.log(error)
+          });
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
