60ef536612fd990364a56d982f50398c09371d1c
khanhtungmtp <umlatao97@gmail.com>
2019-04-28 11:16:51 +0700

    init section 12 directive in vuejs

---
+++ section 12/Directives/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <h1>Default Build directive</h1>
+      <p v-text="'appen text in p tag'"></p>
+      <hr>
+      <p v-html="'<h1>appen html h1 tag in p tag</h1>'"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {}
+</script>
+
+<style>
+
+</style>
