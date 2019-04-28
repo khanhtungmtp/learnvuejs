1398cf419a6f76683c536613e72d4238859d02f3
khanhtungmtp <umlatao97@gmail.com>
2019-04-28 22:08:45 +0700

    Interceptor với request

--- section 15/thao tac voi server/src/main.js
+++ section 15/thao tac voi server/src/main.js
@@ -4,6 +4,15 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 Vue.http.options.root = "https://learnvuejs-76db0.firebaseio.com/data.json"
+
+Vue.http.interceptors.push( (request,next) => {
+  console.log(request);
+  // nếu request là post thì can thiệp đổi thành put (Tức là update)
+  if (request.method =='POST'){
+    request.method ='PUT'
+  }
+  next();
+})
 new Vue({
   el: '#app',
   render: h => h(App)
