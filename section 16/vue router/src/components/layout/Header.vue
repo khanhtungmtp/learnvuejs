aff70ff24649296f6b8954e26df94e7ab8b22cc5
khanhtungmtp <umlatao97@gmail.com>
2019-04-29 14:37:50 +0700

    router link

--- section 16/vue router/src/components/layout/Header.vue
+++ section 16/vue router/src/components/layout/Header.vue
@@ -1,21 +1,28 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <router-link class="nav-link" tag="a" to="/">Home <span class="sr-only">(current)</span></router-link>
-
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" tag="a" to="/user">User</router-link>
-
-        </li>
-
+        <router-link
+            class="nav-item"
+            tag="li"
+            to="/"
+            active-class="active"
+            exact
+        >
+          <a class="nav-link">Home</a>
+        </router-link>
+        <router-link
+          class="nav-item"
+          tag="li"
+          to="/user"
+          active-class="active"
+        ><a class="nav-link">User</a></router-link>
       </ul>
       <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
@@ -26,9 +33,9 @@
 </template>
 
 <script>
-    export default {
-        name: "Header"
-    }
+  export default {
+    name: "Header"
+  }
 </script>
 
 <style scoped>
