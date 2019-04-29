a995dfb9d902cf42e927c49b6701efe8323f692a
khanhtungmtp <umlatao97@gmail.com>
2019-04-29 23:34:00 +0700

    Điều hướng routes con

--- section 16/vue router/src/components/user/UserBegin.vue
+++ section 16/vue router/src/components/user/UserBegin.vue
@@ -1,11 +1,45 @@
 <template>
+  <div>
     <h1>User start from here</h1>
+    <ul class="list-group">
+      <router-link
+        class="list-group-item"
+        tag="li"
+        to="/user/1"
+        style="cursor: pointer"
+      >User 1</router-link>
+      <router-link
+        class="list-group-item"
+        tag="li"
+        to="/user/2"
+        style="cursor: pointer"
+      >User 2</router-link>
+      <router-link
+        class="list-group-item"
+        tag="li"
+        to="/user/3"
+        style="cursor: pointer"
+      >User 3</router-link>
+      <router-link
+        class="list-group-item"
+        tag="li"
+        to="/user/4"
+        style="cursor: pointer"
+      >User 4</router-link>
+      <router-link
+        class="list-group-item"
+        tag="li"
+        to="/user/5"
+        style="cursor: pointer"
+      >User 5</router-link>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "UserBegin"
-    }
+  export default {
+    name: "UserBegin"
+  }
 </script>
 
 <style scoped>
