de9215d2783739f225d7a6d11f4d18d5398d01e8
khanhtungmtp <umlatao97@gmail.com>
2019-04-30 19:30:45 +0700

    Scrollbehavior

--- section 16/vue router/src/main.js
+++ section 16/vue router/src/main.js
@@ -3,14 +3,24 @@ import App from './App.vue'
 
 import VueRouter from 'vue-router'
 // do là đối tượng nên có { }
-import { routes } from "./routes.js";
+import {routes} from "./routes.js";
 
 Vue.use(VueRouter)
 // global router
 const router = new VueRouter({
   // để xem # trên trình duyệt là 1 hash fragments, ko phải ảo
-  mode:'history',
-  routes
+  mode: 'history',
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    // nếu vị trí đã lưu thì trả về vị trí đã lưu
+    if (savedPosition) {
+      return savedPosition;
+    }
+    // trang nào có hash thì nó sẽ nhảy vào hash
+    if (to.hash) {
+      return {selector: to.hash}
+    }
+  }
 })
 
 
