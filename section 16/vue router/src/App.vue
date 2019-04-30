888f01a45e6655f5c990f637c3b81bb46f55b5e7
khanhtungmtp <umlatao97@gmail.com>
2019-04-30 18:45:17 +0700

    Sử dụng transition khi routes

--- section 16/vue router/src/App.vue
+++ section 16/vue router/src/App.vue
@@ -2,8 +2,10 @@
   <div>
     <router-view name="page-header"></router-view>
     <div class="container">
-      <!--  default home    -->
-      <router-view/>
+      <transition name="tung">
+        <!--  default home    -->
+        <router-view/>
+      </transition>
     </div>
   </div>
 </template>
@@ -19,5 +21,22 @@
 </script>
 
 <style>
+  .tung-leave-active{
+    transition: opacity 0.5s ease;
+    opacity: 0;
+    animation: slide-out 0.5s ease-out forwards;
+  }
+  .tung-leave{
+    opacity: 1;
+    transform: translate(0);
+  }
+  @keyframes slide-out {
+    0%{
+      transform: translateX(0);
+    }
+    100%{
+      transform: translateX(30px);
+    }
 
+  }
 </style>
