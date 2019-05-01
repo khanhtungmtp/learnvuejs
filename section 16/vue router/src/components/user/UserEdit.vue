63ce8b357abd836c045476407ab18927ee8bc694
khanhtungmtp <umlatao97@gmail.com>
2019-05-01 10:18:23 +0700

    tối ưu hóa route với lazy load

--- section 16/vue router/src/components/user/UserEdit.vue
+++ section 16/vue router/src/components/user/UserEdit.vue
@@ -1,21 +1,33 @@
 <template>
-   <div>
-     <h1>User edit</h1>
-     <ul>
-       <li>locate: {{ $route.query.locate }}</li>
-       <li>locate: {{ $route.query.browser }}</li>
-     </ul>
-
-     <div style="height: 700px"></div>
-     <h3 id="helloFragment">Sử dụng hash fragments</h3>
-     <div style="height: 1700px"></div>
-   </div>
+  <div>
+    <h1>User edit</h1>
+    <ul>
+      <li>locate: {{ $route.query.locate }}</li>
+      <li>locate: {{ $route.query.browser }}</li>
+    </ul>
+    <button
+      class="btn btn-danger"
+      @click="show">Click để thay đổi
+    </button>
+    <div style="height: 700px"></div>
+    <h3 id="helloFragment">Sử dụng hash fragments</h3>
+    <div style="height: 1700px"></div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "UserEdit"
-    }
+  export default {
+    data() {
+      return {
+        status: false
+      }
+    }, methods: {
+      show() {
+
+      }
+    },
+
+  }
 </script>
 
 <style scoped>
--- section 16/vue router/src/routes.js
+++ section 16/vue router/src/routes.js
@@ -1,10 +1,39 @@
 // define router
 import PageHeader from './components/layout/Header.vue';
 import Home from './components/Home.vue';
-import User from './components/user/User.vue';
-import UserDetail from './components/user/UserDetail.vue';
-import UserStart from './components/user/UserBegin';
-import UserEdit from './components/user/UserEdit';
+
+// sử dụng promise, cái nào yêu cầu mới sử dụng, lazy load
+const User = resolve => {
+  // 'user' để chỉ load 1 file build.js giống như cache, có rồi ko load nữa
+  require.ensure(['./components/user/User.vue'], () => {
+    resolve(require('./components/user/User.vue'))
+  }, 'user')
+}
+
+const UserDetail = resolve => {
+  // ensure là đảm bảo, resolve là giải quyết
+  require.ensure(['./components/user/UserDetail.vue'], () => {
+    resolve(require('./components/user/UserDetail.vue'))
+  }, 'user')
+}
+
+const UserStart = resolve => {
+  require.ensure(['./components/user/UserBegin'], () => {
+    resolve(require('./components/user/UserBegin'))
+  }, 'user')
+}
+
+const UserEdit = resolve => {
+  require.ensure(['./components/user/UserEdit'], () => {
+    resolve(require('./components/user/UserEdit'))
+  }, 'user')
+}
+// import User from './components/user/User.vue';
+// import UserDetail from './components/user/UserDetail.vue';
+// import UserStart from './components/user/UserBegin';
+// import UserEdit from './components/user/UserEdit';
+
+
 import Error from './components/404';
 
 export const routes = [
@@ -17,10 +46,12 @@ export const routes = [
   {
     path: '/user', name: 'user', component: User, children: [
       {path: '', name: 'index', component: UserStart},
-      {path: ':id', name: 'userdetails', component: UserDetail,beforeEnter:(to,from,next)=>{
+      {
+        path: ':id', name: 'userdetails', component: UserDetail, beforeEnter: (to, from, next) => {
           console.log('action guard useredit');
           next();
-        }},
+        }
+      },
       {path: ':id/edit', name: 'useredit', component: UserEdit},
     ]
   },
